c7eaa0fb1ca654bdf88473ef25ca92c121ecdbc1
o-trela <boskichlopaczyna@gmail.com>
2025-06-16 21:24:21 +0200

    Fix tests in report-service

--- report-service/build.gradle.kts
+++ report-service/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
+	testImplementation("com.h2database:h2:2.3.232")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- report-service/src/test/kotlin/pl/edu/p/lodz/wiarygodnik/ReportApplicationTests.kt
+++ report-service/src/test/kotlin/pl/edu/p/lodz/wiarygodnik/ReportApplicationTests.kt
@@ -2,8 +2,9 @@ package pl.edu.p.lodz.wiarygodnik
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import pl.edu.p.lodz.wiarygodnik.report.ReportApplication
 
-@SpringBootTest
+@SpringBootTest(classes = [ReportApplication::class])
 class ReportApplicationTests {
 
 	@Test
